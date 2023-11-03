d5a69183bb0e40202ef04093368723a3e5384cd2
Eli Wood <mreliwood1@gmail.com>
2023-11-03 02:16:29 -0700

    completed footer design, still need to restyle buttons

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,8 +5,8 @@ import { useEffect } from 'react';
 import Link from 'next/link';
 
 // internal
-import { Login } from '@/components';
-import { verifyToken } from '@/utils';
+import { Button, Login } from '@/components';
+import { links, verifyToken, worklist } from '@/utils';
 
 // state
 import { useSelector, useDispatch } from 'react-redux';
@@ -46,24 +46,136 @@ export default function Footer() {
 
 	return (
 		<footer className={styles.footer}>
-			<Link href='/bibliography' className={styles.site_title}>
-				<p>Bibliography</p>
-			</Link>
+			<div className={styles.footer_logo}>
+				<h1 className={styles.footer_title}>Mary Elene Wood</h1>
 
-			{token ? (
-				<div className={styles.user_nav}>
-					<Link href='/cpw'>
-						<button>Change Password</button>
-					</Link>
-					<button onClick={handleLogoutClick}>Logout</button>
+				<div className={styles.footer_caption_container}>
+					<h4 className={styles.footer_caption}>Writer.</h4>
+					<h4 className={styles.footer_caption}>Teacher.</h4>
+					<h4 className={styles.footer_caption}>Scholar.</h4>
 				</div>
-			) : (
-				<p className={styles.text_link} onClick={handleLoginClick}>
-					Login
-				</p>
-			)}
+			</div>
 
-			{loginVisible && <Login />}
+			<div className={styles.footer_content}>
+				<section className={styles.column}>
+					<h3 className={styles.column_title}>Site Map</h3>
+
+					<hr />
+
+					<div className={styles.column_content}>
+						<ul className={styles.column_content_section}>
+							<li>
+								<Link href='/'>Home</Link>
+							</li>
+							<li>
+								<Link href='/writing'>Writing</Link>
+							</li>
+							<li>
+								<Link href='/teaching'>Teaching</Link>
+							</li>
+							<li>
+								<Link href='/blog'>Blog</Link>
+							</li>
+							<li>
+								<Link href='/contact'>Contact</Link>
+							</li>
+							<li>
+								<Link href='/bibliography'>Bibliography</Link>
+							</li>
+						</ul>
+					</div>
+				</section>
+
+				<section className={styles.column}>
+					<h3 className={styles.column_title}>Contact</h3>
+
+					<hr />
+
+					<div className={styles.column_content}>
+						<div className={styles.column_content_section}>
+							<Link href={links.phone}>(541) 346-3010</Link>
+							<Link href={links.email}>mewood@uoregon.edu</Link>
+						</div>
+
+						<div className={styles.column_content_section}>
+							<Link href='https://www.uoregon.edu' target='_blank'>
+								uoregon.edu
+							</Link>
+						</div>
+					</div>
+				</section>
+
+				<section className={styles.column}>
+					<h3 className={styles.column_title}>Admin</h3>
+
+					<hr />
+
+					<div className={styles.column_content}>
+						<div className={styles.column_content_section}>
+							<p>© 2023 Mary Elene Wood.</p>
+							<p>All Rights Reserved.</p>
+						</div>
+
+						<div className={styles.column_content_section}>
+							<div className={styles.section_row}>
+								<p>
+									Website by{' '}
+									<Link href='https://eliwooddesign.com' target='_blank'>
+										Eli Wood
+									</Link>
+									.
+								</p>
+							</div>
+						</div>
+
+						<div className={styles.column_content_section}>
+							<div className={styles.user_nav}>
+								{token ? (
+									<>
+										<Link href='/cpw'>
+											<button>Change Password</button>
+										</Link>
+										<button onClick={handleLogoutClick}>Logout</button>
+									</>
+								) : (
+									<Button.TextArrow href='/login'>Login</Button.TextArrow>
+								)}
+							</div>
+
+							{loginVisible && <Login />}
+						</div>
+					</div>
+				</section>
+
+				<section className={styles.column}>
+					<h3 className={styles.column_title}>External Sources</h3>
+
+					<hr />
+
+					<div className={styles.column_content}>
+						<ul className={styles.column_content_section}>
+							{worklist.map((work) => {
+								const { link } = work;
+								if (!link) return;
+
+								const key = Math.floor(Math.random() * 1000000);
+
+								const linkArray = link.split('/');
+								const linkText = linkArray[2];
+								const href = 'http://' + linkText;
+
+								return (
+									<li key={key}>
+										<Link href={href} target='_blank'>
+											{linkText}
+										</Link>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</section>
+			</div>
 		</footer>
 	);
 }
